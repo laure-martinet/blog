2c884295351ab3d16047bfb2b8ae92d54b95f35f
Martinet Laure <laure.martinet@laplateforme.io>
2021-11-30 13:52:15 +0100

    footer button

--- footer.php
+++ footer.php
@@ -2,5 +2,12 @@
     <div class="footer">
         <div class="boutonsfooter">
             <input type="button" href="index.php" value="Accueil">
+            <input type="button" href="connexion.php" value="Connexion">
+            <input type="button" href="inscription.php" value="Inscription">
+            <input type="button" href="profil.php" value="profil">
+            <input type="button" href="articles.php" value="Ensemble articles">
+            <input type="button" href="creer-article.php" value="Crée article">
+            <input type="button" href="article.php" value="Voir articles et com">
+            <img src=""
         </div>
 </div>
