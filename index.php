4d8eb941c3d887d0ebde21b4c4c41f32f1c3d0de
alex <alex.lovati@laplateforme.io>
2021-11-30 16:24:40 +0100

    commit 1index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="style.css" rel="stylesheet">
+        <title>Blog</title>
+    </head>
+    <body id="al_body">
+            
+        <header>
+        
+        </header>
+        <main id="al_main">
+            <h1 id="al_h1"> Dernier articles mis en ligne</h1>
+            <div class="rectangle"></div>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
