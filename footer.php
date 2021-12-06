42559d4e97b7a4ef5d9d890e15087ca9078326e5
Laura <laura.rodriguez@laplateforme.io>
2021-12-06 11:59:32 +0100

    liens footer et faute inscription

--- footer.php
+++ footer.php
@@ -3,19 +3,18 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="style.css">
-        <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
 <footer>
     <div class="footerbox">
         <div class="boutonsfooter">
-            <input class="btn btn-secondary btn-lg" type="button" href="index.php" value="Accueil">
-            <input class="btn btn-secondary btn-lg" type="button" href="connexion.php" value="Connexion">
-            <input class="btn btn-secondary btn-lg" type="button" href="inscription.php" value="Inscription">
-            <input class="btn btn-secondary btn-lg" type="button" href="profil.php" value="Profil">
-            <input class="btn btn-secondary btn-lg" type="button" href="articles.php" value="Ensemble articles">
-            <input class="btn btn-secondary btn-lg" type="button" href="creer-article.php" value="Crée article">
-            <input class="btn btn-secondary btn-lg" type="button" href="article.php" value="Voir articles et com">
+            <a class="btn btn-secondary btn-lg" href="index.php"> Accueil</a>
+            <a class="btn btn-secondary btn-lg" href="inscription.php"> Inscription</a>
+            <a class="btn btn-secondary btn-lg" href="connexion.php"> Connexion</a>
+            <a class="btn btn-secondary btn-lg" href="profil.php"> Profil</a>
+            <a class="btn btn-secondary btn-lg" href="articles.php"> Ensembles articles</a>
+            <a class="btn btn-secondary btn-lg" href="creer-article.php"> Créer un article</a>
+            <a class="btn btn-secondary btn-lg" href="article.php"> Voir articles et comms</a>
         </div>
     </div>
     <div id="imagegithub">
