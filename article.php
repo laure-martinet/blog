4387aa93d6fc75b742a642e7b73827a96ef1dbb3
Laura <laura.rodriguez@laplateforme.io>
2021-12-07 15:46:42 +0100

    article.php qui marche

--- article.php
+++ article.php
@@ -9,7 +9,7 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
     if($article->rowCount() == 1) {
         $article = $article->fetch();
         $titre = $article['titre'];
-        $contenu = $article['contenu'];
+        $contenu = $article['article'];
     } else {
         die('Cet article n\'existe pas !');
     }
@@ -23,8 +23,27 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
     <title>Accueil</title>
     <meta charset="utf-8">
 </head>
-<body>
-    <h1><?= $titre ?></h1>
-    <p><?= $contenu ?></p>
+
+<body id="al_body">
+<header>
+    <?php
+        include_once ("header.php");
+    ?>
+</header>
+<main>
+    <p class="lr_h2">Catégorie : <?php echo $article['id_categorie'] ;?></p>
+    <div id="deplacement_article"> 
+        <div id="lr_article"> 
+            <p class="lr_h2">Créée le <?php echo $article['date'] ;?></p>
+            <h1 class="lr_h2"><?= $titre ?></h1>
+            <p class="lr_h2"><?= $contenu ?></p>
+        </div>
+    </div>
+</main>
+<footer>
+    <?php
+        include_once ("footer.php");
+    ?>
+</footer>
 </body>
 </html>
