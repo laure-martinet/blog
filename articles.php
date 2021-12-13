c4865a983057fd9dea042745bba6ab7419b30fe5
alex <alex.lovati@laplateforme.io>
2021-12-13 16:39:48 +0100

    Auto stash before merge of "master" and "origin/master"

--- articles.php
+++ articles.php
@@ -2,6 +2,18 @@
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', ''); 
 $articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC');
+
+$count=$bdd->prepare("select count(id) as cpt from articles");
+$count->setFetchMode(PDO::FETCH_ASSOC);
+$count->execute();
+$tcount=$count->fetchAll();
+
+
+$nbr_par_page=5;
+$nbr_par_page= ceil ($tcount[0]["cpt"]/$nbr_par_page);
+
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,11 +29,16 @@ $articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC');
             ?>
         </header>
         <main>
-            <ul class=liste>
+            <ul id=al_articles>
             <?php while($a = $articles->fetch()) { ?>
-            <li><a href="article.php?id=<?= $a['id'] ?>"><?= $a['titre'] ?></a></li>
+            <li><a class=al_liste href="article.php?id=<?= $a['id'] ?>"><?= $a['titre'] ?></a></li>
             <?php } ?>
             </ul>
+            <?php
+                for($i=1;$i<=$nbr_par_page;$i++){
+                    echo "<a href=''>$i</a>";
+                }
+            ?>
         </main>
         <footer>
             <?php
