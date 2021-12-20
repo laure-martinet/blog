9ad7df398c7bbe3556b3c66edbe6c51e944c44dd
Martinet Laure <laure.martinet@laplateforme.io>
2021-12-20 10:57:03 +0100

    edition ca marche tjr pas

--- editionarticle.php
+++ editionarticle.php
@@ -1,26 +1,25 @@
 <?php
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+if (isset($_POST['newarticle']) && !empty($_POST['newarticle'])) {
+  $idchange = $_POST['id'];
+  $article = $_POST['newarticle'];
+  $titre = $_POST['newtitre'];
+  $requetearticle = $bdd->prepare("SELECT * FROM articles WHERE article = ?, titre = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
+  $requetearticle->execute(array($article, $titre));
+  $articleexist = $requetearticle->rowCount(); // rowCount = Si une ligne existe = PAS BON
 
-if(isset($_GET['id']) AND !empty($_GET['id'])){
-$getid = $_GET['id'];
+  if ($articleexist !== 0) {
+      $msg = "L'article existe déjà !";
+  } else {
 
-$recuparticle = $bdd->prepare('SELECT * FROM articles WHERE id = ?');
-$recuparticle->execute(array($getid));
-// TOUT CA PERMET DE RECUP L'ARTICLE DE L'ID UTILISATEUR
-if($recuparticle->rowCount() > 0){
-$articleInfos = $recuparticle->fetch();
-$titre = $articleInfos['titre'];
-$contenu = $articleInfos['article'];
-
-if(isset($_POST['Valider'])){
-$titre_saisie = htmlspecialchars($_POST['titre']);
-$contenu_saisie = htmlspecialchars($_POST['article']);
-
-$updatearticle = $bdd->prepare('UPDATE articles SET titre = ? AND article = ? WHERE id = ?');
-$updatearticle->execute(array($titre_saisie, $contenu_saisie, $getid));
-}
-}
+      $newarticle = htmlspecialchars($_POST['article']);
+      $newtitre = htmlspecialchars($_POST['titre']);
+      $insertlogin = $bdd->prepare("UPDATE articles SET article = ?, titre = ? WHERE id = ?");
+      $insertlogin->execute(array($newarticle, $newtitre, $idchange));
+      header('Location: editionarticle.php');
+      exit();
+  }
 }
 
 ?>
@@ -38,7 +37,7 @@ include_once('header.php');
 ?>
 </header>
 <main id="al_main">
-    <form id="form_inscription" method="POST">
+    <form id="form_inscription" method="post">
   <div id="LMedition">
 <?php
 $recuparticle = $bdd->query('SELECT * FROM articles');
@@ -50,7 +49,7 @@ while ($article = $recuparticle->fetch()){
       <textarea name="article_contenu" placeholder="Contenu de l'article"><?= $article['article'] ?></textarea>
 
         <a href="supprimerarticle.php?id=<?= $article['id'] ?>">Supprimer</a>
-        <input type="submit" name="Valider">
+        <input type="submit" name="Valider" value="Valider">
     </div>
 </form>
     <?php
