cd5fc00b0ac366597549696432427c22ec749b09
alex <alex.lovati@laplateforme.io>
2021-12-21 18:07:13 +0100

    pagination fini et easter egg '-'

--- articles.php
+++ articles.php
@@ -2,20 +2,43 @@
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
 
-$article_par_page = 5;
-$articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC LIMIT 0,5');
-$articles_totales = $articles->rowCount();
+// $article_par_page = 5;
+// $articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC LIMIT 5,5');
+// $articles_totales = $articles->rowCount();
 
-$pagestotales = ceil($articles_totales / $article_par_page);
+// $pagestotales = ceil($articles_totales / $article_par_page);
 
-if (isset($_GET['id_categorie']) and !empty($_GET['id_categorie']) and $_GET['id_categorie'] > 0 and $_GET['id_categorie'] <= $pagestotales) {
-    $_GET['id_categorie'] = intval($_GET['id_categorie']);
-    $pageCourante = $_GET['id_categorie'];
-} else {
-    $pageCourante = 1;
+// if (isset($_GET['id_categorie']) and !empty($_GET['id_categorie']) and $_GET['id_categorie'] > 0 and $_GET['id_categorie'] <= $pagestotales) {
+//     $_GET['id_categorie'] = intval($_GET['id_categorie']);
+//     $pageCourante = $_GET['id_categorie'];
+// } else {
+//     $pageCourante = 1;
+// }
+// $depart = ($pageCourante - 1) * $article_par_page;
+if (isset($_GET['page'])&& !empty($_GET['page'])){
+    $pageCourante=(int)strip_tags($_GET['page']);
+}else{
+    $pageCourante=1;
 }
-$depart = ($pageCourante - 1) * $article_par_page;
 
+$sql= 'SELECT COUNT(*) AS nb_articles FROM `articles`;';
+$query=$bdd->prepare($sql);
+$query->execute();
+$result=$query->fetch();
+$nbArticles=(int) $result['nb_articles'];
+
+$article_par_page=5;
+
+$pagestotales = ceil($nbArticles / $article_par_page);
+$debut=($pageCourante*$article_par_page)-$article_par_page;
+
+$sql='SELECT * FROM articles ORDER BY date DESC LIMIT :debut, :article_par_page;';
+$query=$bdd->prepare($sql);
+$query-> bindValue(':debut', $debut, PDO ::PARAM_INT);
+$query-> bindValue(':article_par_page', $article_par_page, PDO ::PARAM_INT);
+$query->execute();
+
+$articles=$query->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <!DOCTYPE html>
@@ -23,8 +46,10 @@ $depart = ($pageCourante - 1) * $article_par_page;
 
 <head>
     <title>articles</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../style.css" />
     <meta charset="utf-8">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body id="al_body">
@@ -35,34 +60,45 @@ $depart = ($pageCourante - 1) * $article_par_page;
     </header>
     <main id=al_articles>
         <?php
-        $articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC LIMIT ' . $depart . ',' . $article_par_page);
-        while ($art = $articles->fetch()) {
+        // $articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC LIMIT ' . $depart . ',' . $article_par_page);
+        // while ($art = $articles->fetch()) {
+            foreach ($articles as $article){
         ?>
-            <div id="al_article">
-                <li><a class="al_href" href="article.php?id=<?= $art['id'] ?>"><?= $art['titre'] ?></a></li>
-                <i id="al_date"><?php echo $art['date']; ?></i>
-            </div>
-            <br /><br />
+        <?= $article['id']?>
+        <a href="articles.php?p="><?= $article['titre']?> </a><br>
+        <?= $article['date']?><br>
+            
         <?php
         }
         ?>
         <?php
        
-        for($i=1; $i<= $pagestotales; $i++) {
-            if ($i == $pageCourante) {
-                echo $i . '' ;
+        // for($i=1; $i<= $pagestotales; $i++) {
+        //     if ($i == $pageCourante) {
+        //         echo $i . '' ;
                 
 
-            }
-            else {
-                // echo '<a href="articles.php?p=' . $i . '">' . $i . '</a> ';
-            }
-        }
+        //     }
+        //     else {
+        //         echo '<a href="articles.php?p=' . $i . '">' . $i . '</a> ';
+        //     }
+        // }
         ?>
-        <a href="?id_categorie=<?php echo $i - 1; ?>">Page précédente</a>
-        <a href="?id_categorie=<?php echo $i ++; ?>">Page suivante</a>
-        <
-        <
+        <nav>
+            <ul class="pagination">
+                <li class="page-item <?=($pageCourante == 1) ? "disabled" :"" ?>">
+                    <a href="articles.php?page=<?= $pageCourante-1 ?>" class="page-link">Précédente</a>
+                </li> 
+                <?php for($page = 1; $page <= $pagestotales; $page++): ?>
+                            <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                                <a href="articles.php?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                            </li>
+                        <?php endfor ?>
+                <li class="page-item <?=($pageCourante==$pagestotales) ? "disabled" :"" ?>">
+                    <a href="articles.php?page=<?= $pageCourante+1 ?>" class="page-link">Suivante</a>
+                </li>
+            </ul>
+        </nav>
     </main>
     <footer>
         <?php
