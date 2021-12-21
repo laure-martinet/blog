ac172d60d56bb71d1acd49b823655a64c62b9c85
Laura <laura.rodriguez@laplateforme.io>
2021-12-21 18:11:44 +0100

    petite modif css mise en page pas fini

--- admin.php
+++ admin.php
@@ -9,65 +9,65 @@ $lis = $listedroits->fetchAll();
 
 // ID nécessaire pour la connexion 
 if (!isset($_SESSION['id']) || $_SESSION['id_droits'] != 1337) {
-    header("Location: profil.php");
-    exit();
+   header("Location: profil.php");
+   exit();
 }
 
 // Fonction supprimé un utilisateur
 if (isset($_GET['supprimer']) && !empty($_GET['supprimer'])) {
-    $supprimer = (int) $_GET['supprimer'];
-    $req = $bdd->prepare('DELETE FROM utilisateurs WHERE id = ?');
-    $req->execute(array($supprimer));
-    header("Location: admin.php");
-    exit();
+   $supprimer = (int) $_GET['supprimer'];
+   $req = $bdd->prepare('DELETE FROM utilisateurs WHERE id = ?');
+   $req->execute(array($supprimer));
+   header("Location: admin.php");
+   exit();
 }
 
 // Fonction Modifié le login d'un utilisateur
 if (isset($_POST['newlogin']) && !empty($_POST['newlogin'])) {
-    $idchange = $_POST['id'];
-    $login = $_POST['newlogin'];
-    $requetelogin = $bdd->prepare("SELECT * FROM utilisateurs WHERE login = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
-    $requetelogin->execute(array($login));
-    $loginexist = $requetelogin->rowCount(); // rowCount = Si une ligne existe = PAS BON
+   $idchange = $_POST['id'];
+   $login = $_POST['newlogin'];
+   $requetelogin = $bdd->prepare("SELECT * FROM utilisateurs WHERE login = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
+   $requetelogin->execute(array($login));
+   $loginexist = $requetelogin->rowCount(); // rowCount = Si une ligne existe = PAS BON
 
-    if ($loginexist !== 0) {
-        $msg = "Le login existe déjà !";
-    } else {
-        $newlogin = htmlspecialchars($_POST['newlogin']);
-        $insertlogin = $bdd->prepare("UPDATE utilisateurs SET login = ? WHERE id = ?");
-        $insertlogin->execute(array($newlogin, $idchange));
-        header('Location: admin.php');
-        exit();
-    }
+   if ($loginexist !== 0) {
+      $msg = "Le login existe déjà !";
+   } else {
+      $newlogin = htmlspecialchars($_POST['newlogin']);
+      $insertlogin = $bdd->prepare("UPDATE utilisateurs SET login = ? WHERE id = ?");
+      $insertlogin->execute(array($newlogin, $idchange));
+      header('Location: admin.php');
+      exit();
+   }
 }
 // Fonction modifié l'email d'un utilisateur 
 if (isset($_POST['newmail']) && !empty($_POST['newmail'])) {
-    $idchange = $_POST['id'];
-    $email = $_POST['newmail'];
-    $requetemail = $bdd->prepare("SELECT * FROM utilisateurs WHERE email = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
-    $requetemail->execute(array($email));
-    $emailexist = $requetemail->rowCount(); // rowCount = Si une ligne existe = PAS BON
+   $idchange = $_POST['id'];
+   $email = $_POST['newmail'];
+   $requetemail = $bdd->prepare("SELECT * FROM utilisateurs WHERE email = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
+   $requetemail->execute(array($email));
+   $emailexist = $requetemail->rowCount(); // rowCount = Si une ligne existe = PAS BON
 
-    if ($emailexist !== 0) {
-        $msg = "L'email existe déjà !";
-    } else {
+   if ($emailexist !== 0) {
+      $msg = "L'email existe déjà !";
+   } else {
 
-        $newmail = htmlspecialchars($_POST['newmail']);
-        $insertlogin = $bdd->prepare("UPDATE utilisateurs SET email = ? WHERE id = ?");
-        $insertlogin->execute(array($newmail, $idchange));
-        header('Location: admin.php');
-        exit();
-    }
+      $newmail = htmlspecialchars($_POST['newmail']);
+      $insertlogin = $bdd->prepare("UPDATE utilisateurs SET email = ? WHERE id = ?");
+      $insertlogin->execute(array($newmail, $idchange));
+      header('Location: admin.php');
+      exit();
+   }
 }
 // Fonction modifié le rang d'un utilisateur
 if (isset($_POST['select'])) {
 
-    $idchange = $_POST['id'];
-    $rang = $_POST['select'];
-    $changerrang = $bdd->prepare("UPDATE utilisateurs SET id_droits = ? WHERE id = ?");
-    $changerrang->execute(array($rang,$idchange));
-    header('Location: admin.php');
-    exit();
+   $idchange = $_POST['id'];
+   $rang = $_POST['select'];
+   $changerrang = $bdd->prepare("UPDATE utilisateurs SET id_droits = ? WHERE id = ?");
+   $changerrang->execute(array($rang, $idchange));
+   header('Location: admin.php');
+   exit();
 }
 
 
@@ -77,68 +77,66 @@ if (isset($_POST['select'])) {
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Espace Administrateur</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <meta charset="UTF-8">
+   <link rel="stylesheet" type="text/css" href="style.css">
+   <title>Espace Administrateur</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body id="al_body">
    <header>
-      <?php 
-            include_once('header.php');
+      <?php
+      include_once('header.php');
       ?>
    </header>
-
    <main id="al_main">
-      <h1 id="LMh1">Espace Administrateur</h1>
-      <br />
-   <table>
-      <thead>
-         <tr class="test">
-            <th class="test">Login</th>
-            <th class="test">Email</th>
-            <th class="test">Droits</th>
-            </thead>
-            <?php while ($u = $utilisateurs->fetch()) { ?>
-               <form id="form_inscription" method="POST">
-
-                  <input id="id" type="hidden" name="id" value="<?php echo $u['idutilisateur']; ?>">
-                  <label class="text-light" for="newlogin"></label>
-                  <td><input class="crtdedition" id="newlogin" type="text" name="newlogin" value="<?php echo $u['login']; ?>"></td>
-                  <label class="text-light" for="newmail"></label>
-                  <td><input class="crtdedition" id="newmail" type="mail" name="newmail" value="<?php echo $u['email']; ?>"></td>
-                  <td>
-                     <select name="select" id="select">
-                           <?php foreach ($lis as $key => $value) { ?>
-                              <option <?= $u['id_droits'] == $value['id'] ? "selected":NULL ?> value="<?= $value['id'] ?>"><?= $value['nom'] ?></option>
-                           <?php
-                           } ?>
-                           <!-- Boucle qui permet de savoir le rang de mon utilisateur -->
-                        </select>
-                  </td>
-                  <td class=test><a class="btn btn-danger" href="admin.php?supprimer=<?= $u['idutilisateur'] ?>">Bannir</a></td>
-                  <td class=test><input id="" type="submit" class="btn btn-primary" name="submit" value="Confirmé !"></td>
-               </form>
-               </tr>
-            <?php } ?>
-   </table>
-<br>
-
-<br>
-   <?php
+      <div id="div_admin">
+      <table id="LMtitrediv">
+         <h1 class="LM_color_text">Espace Administrateur</h1>
+         <thead>
+            <tr>
+               <th class="LM_color_text">Login</th>
+               <th class="LM_color_text">Email</th>
+               <th class="LM_color_text">Droits</th>
+         </thead>
+         <?php while ($u = $utilisateurs->fetch()) { ?>
+            <form method="POST">
+               <input class="text_admin" type="hidden" name="id" value="<?php echo $u['idutilisateur']; ?>">
+               <label class="text_admin" for="newlogin"></label>
+               <td><input class="text_admin" id="newlogin" type="text" name="newlogin" value="<?php echo $u['login']; ?>"></td>
+               <label class="text_admin" for="newmail"></label>
+               <td><input class="text_admin" id="newmail" type="mail" name="newmail" value="<?php echo $u['email']; ?>"></td>
+               <td>
+                  <select name="select" id="select">
+                     <?php foreach ($lis as $key => $value) { ?>
+                        <option <?= $u['id_droits'] == $value['id'] ? "selected" : NULL ?> value="<?= $value['id'] ?>"><?= $value['nom'] ?></option>
+                     <?php
+                     } ?>
+                     <!-- Boucle qui permet de savoir le rang de mon utilisateur -->
+                  </select>
+               </td>
+               <td class=test><a class="btn btn-danger" href="admin.php?supprimer=<?= $u['idutilisateur'] ?>">Bannir</a></td>
+               <td class=test><input type="submit" class="btn btn-primary" name="submit" value="Confirmer !"></td>
+            </form>
+            </tr>
+         <?php } ?>
+      </table>
+      <br>
+      <br>
+      <?php
       if (isset($msg)) {
-            echo '<font color="pink">' . $msg . '</font><br /><br />';
+         echo '<font color="pink">' . $msg . '</font><br /><br />';
       }
-   ?>
-  <a href="deconnexion"><input type="button" value="Déconnexion"></a>
-    <a href="creer-article.php"><input type="button" value="Crée article"></a>
-    <a href="editionarticle.php"><input type="button" value="Modifier article"></a>
-</main>
+      ?>
+      <a href="deconnexion"><input class="btn btn-secondary btn-lg" type="button" value="Déconnexion"></a>
+      <a href="creer-article.php"><input class="btn btn-secondary btn-lg" type="button" value="Crée article"></a>
+      <a href="editionarticle.php"><input class="btn btn-secondary btn-lg" type="button" value="Modifier article"></a>
+      </div>
+   </main>
    <footer>
-   <?php
+      <?php
       include_once('footer.php');
-   ?>
+      ?>
    </footer>
 </body>
 </head>
--- footer.php
+++ footer.php
@@ -6,8 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
 <footer>
-    <div class="footerbox">
-        <div class="boutonsfooter">
+    <div id="footerbox">
             <a class="btn btn-secondary btn-lg" href="index.php"> Accueil</a>
             <a class="btn btn-secondary btn-lg" href="inscription.php"> Inscription</a>
             <a class="btn btn-secondary btn-lg" href="connexion.php"> Connexion</a>
@@ -15,9 +14,7 @@
             <a class="btn btn-secondary btn-lg" href="articles.php"> Ensembles articles</a>
             <a class="btn btn-secondary btn-lg" href="creer-article.php"> Créer un article</a>
             <a class="btn btn-secondary btn-lg" href="article.php?id=1"> Voir articles et comms</a>
+            <a href="https://github.com/laure-martinet/blog.git"><img src="medias/unnamed-removebg-preview.png" height="60px" width="60px"></a>
         </div>
     </div>
-    <div id="imagegithub">
-        <a href="https://github.com/laure-martinet/blog.git"><img src="medias/unnamed-removebg-preview.png" height="42%" width="42%"></a>
-        </div>
 </footer>
