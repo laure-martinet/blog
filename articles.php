9088b10bad06364f043458621fb08db1f2b7cc72
Laura <laura.rodriguez@laplateforme.io>
2021-12-22 22:01:41 +0100

    Normalement tout est bon, plus que Plesk

--- articles.php
+++ articles.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+$lacateg= $_GET['categorie'];
 
 if (isset($_GET['page'])&& !empty($_GET['page'])){
     $pageCourante=(int)strip_tags($_GET['page']);
@@ -9,7 +10,6 @@ if (isset($_GET['page'])&& !empty($_GET['page'])){
 }
 
 $sql= 'SELECT COUNT(*) AS nb_articles FROM `articles`;';
-$lacateg= $_GET['id_categorie'];
 $query=$bdd->prepare($sql);
 $query->execute();
 $result=$query->fetch();
@@ -20,7 +20,7 @@ $article_par_page=5;
 $pagestotales = ceil($nbArticles / $article_par_page);
 $debut=($pageCourante*$article_par_page)-$article_par_page;
 
-$sql='SELECT * FROM articles INNER JOIN categories ON articles.id_categorie = categories.id WHERE id_categorie = :id_categorie ORDER BY date DESC LIMIT :debut, :article_par_page;';
+$sql='SELECT *, articles.id as art_id FROM articles INNER JOIN categories ON articles.id_categorie = categories.id WHERE id_categorie = :id_categorie ORDER BY date DESC LIMIT :debut, :article_par_page;';
 $query=$bdd->prepare($sql);
 $query-> bindValue(':debut', $debut, PDO ::PARAM_INT);
 $query-> bindValue(':article_par_page', $article_par_page, PDO ::PARAM_INT);
@@ -37,7 +37,6 @@ $articles=$query->fetchAll(PDO::FETCH_ASSOC);
     <title>articles</title>
     <link rel="stylesheet" href="../style.css" />
     <meta charset="utf-8">
-    <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
@@ -52,7 +51,7 @@ $articles=$query->fetchAll(PDO::FETCH_ASSOC);
             foreach ($articles as $article){
         ?>
         <div id=al_article>
-        <a class="al_href" href="article.php?id=<?= $article['id']?>"><?= $article['titre']?> </a><br>
+        <a class="al_href" href="article.php?id=<?= $article['art_id']?>"><?= $article['titre']?> </a><br>
         <div id="al_date"><?= $article['date']?></div></div><br>
         
         <?php
@@ -65,7 +64,7 @@ $articles=$query->fetchAll(PDO::FETCH_ASSOC);
                 </li> &emsp;
                 <?php for($page = 1; $page <= $pagestotales; $page++): ?>
                             <li class="btn btn-secondary btn-lg <?= ($currentPage == $page) ? "active" : "" ?>">
-                                <a href="articles.php?page=<?= $page ?>"class="btn btn-secondary btn-lg"><?= $page ?></a>
+                                <a href="articles.php?categorie=<?= $article['id_categorie']?>&page=<?= $page ?>"class="btn btn-secondary btn-lg"><?= $page ?></a>
                             </li>
                         <?php endfor ?>&emsp;
                 <li class="btn btn-secondary btn-lg <?=($pageCourante==$pagestotales) ? "disabled" :"" ?>">
