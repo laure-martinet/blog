a8af375b46a9a8f5371d440108a347e2a677a00c
alex <alex.lovati@laplateforme.io>
2021-12-22 14:33:43 +0100

    categorie filtre

--- articles.php
+++ articles.php
@@ -2,19 +2,6 @@
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
 
-// $article_par_page = 5;
-// $articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC LIMIT 5,5');
-// $articles_totales = $articles->rowCount();
-
-// $pagestotales = ceil($articles_totales / $article_par_page);
-
-// if (isset($_GET['id_categorie']) and !empty($_GET['id_categorie']) and $_GET['id_categorie'] > 0 and $_GET['id_categorie'] <= $pagestotales) {
-//     $_GET['id_categorie'] = intval($_GET['id_categorie']);
-//     $pageCourante = $_GET['id_categorie'];
-// } else {
-//     $pageCourante = 1;
-// }
-// $depart = ($pageCourante - 1) * $article_par_page;
 if (isset($_GET['page'])&& !empty($_GET['page'])){
     $pageCourante=(int)strip_tags($_GET['page']);
 }else{
@@ -22,6 +9,7 @@ if (isset($_GET['page'])&& !empty($_GET['page'])){
 }
 
 $sql= 'SELECT COUNT(*) AS nb_articles FROM `articles`;';
+$lacateg= $_GET['id_categorie'];
 $query=$bdd->prepare($sql);
 $query->execute();
 $result=$query->fetch();
@@ -32,10 +20,12 @@ $article_par_page=5;
 $pagestotales = ceil($nbArticles / $article_par_page);
 $debut=($pageCourante*$article_par_page)-$article_par_page;
 
-$sql='SELECT * FROM articles ORDER BY date DESC LIMIT :debut, :article_par_page;';
+// $sql='SELECT * FROM articles ORDER BY date DESC LIMIT :debut, :article_par_page;';
+$sql='SELECT * FROM articles INNER JOIN categories ON articles.id_categorie = categories.id WHERE id_categorie = :id_categorie ORDER BY date DESC LIMIT :debut, :article_par_page;';
 $query=$bdd->prepare($sql);
 $query-> bindValue(':debut', $debut, PDO ::PARAM_INT);
 $query-> bindValue(':article_par_page', $article_par_page, PDO ::PARAM_INT);
+$query-> bindValue(':id_categorie', $lacateg, PDO ::PARAM_INT);
 $query->execute();
 
 $articles=$query->fetchAll(PDO::FETCH_ASSOC);
@@ -60,42 +50,27 @@ $articles=$query->fetchAll(PDO::FETCH_ASSOC);
     </header>
     <main id=al_articles>
         <?php
-        // $articles = $bdd->query('SELECT * FROM articles ORDER BY date DESC LIMIT ' . $depart . ',' . $article_par_page);
-        // while ($art = $articles->fetch()) {
             foreach ($articles as $article){
         ?>
-        <?= $article['id']?>
-        <a href="articles.php?p="><?= $article['titre']?> </a><br>
-        <?= $article['date']?><br>
-            
+        <div id=al_article>
+        <a class="al_href" href="article.php?id=<?= $article['id']?>"><?= $article['titre']?> </a><br>
+        <div id="al_date"><?= $article['date']?></div></div><br>
+        
         <?php
         }
         ?>
-        <?php
-       
-        // for($i=1; $i<= $pagestotales; $i++) {
-        //     if ($i == $pageCourante) {
-        //         echo $i . '' ;
-                
-
-        //     }
-        //     else {
-        //         echo '<a href="articles.php?p=' . $i . '">' . $i . '</a> ';
-        //     }
-        // }
-        ?>
-        <nav>
+        <nav id= al_boutonart >
             <ul class="pagination">
-                <li class="page-item <?=($pageCourante == 1) ? "disabled" :"" ?>">
-                    <a href="articles.php?page=<?= $pageCourante-1 ?>" class="page-link">Précédente</a>
-                </li> 
+                <li class="btn btn-secondary btn-lg <?=($pageCourante == 1) ? "disabled" :"" ?>">
+                    <a href="articles.php?page=<?= $pageCourante-1 ?>" class="btn btn-secondary btn-lg">Précédente</a>
+                </li> &emsp;
                 <?php for($page = 1; $page <= $pagestotales; $page++): ?>
-                            <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
-                                <a href="articles.php?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                            <li class="btn btn-secondary btn-lg <?= ($currentPage == $page) ? "active" : "" ?>">
+                                <a href="articles.php?page=<?= $page ?>"class="btn btn-secondary btn-lg"><?= $page ?></a>
                             </li>
-                        <?php endfor ?>
-                <li class="page-item <?=($pageCourante==$pagestotales) ? "disabled" :"" ?>">
-                    <a href="articles.php?page=<?= $pageCourante+1 ?>" class="page-link">Suivante</a>
+                        <?php endfor ?>&emsp;
+                <li class="btn btn-secondary btn-lg <?=($pageCourante==$pagestotales) ? "disabled" :"" ?>">
+                    <a href="articles.php?page=<?= $pageCourante+1 ?>" class="btn btn-secondary btn-lg">Suivante</a>
                 </li>
             </ul>
         </nav>
